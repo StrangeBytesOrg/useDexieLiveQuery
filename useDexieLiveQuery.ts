08799d44295c4ca11c8d8866cb4d7aa62e353508
StrangeBytesDev <dev@strangebytes.org>
2025-03-15 15:37:04 -0700

    Update useDexieLiveQuery to use async initial load

--- useDexieLiveQuery.ts
+++ useDexieLiveQuery.ts
@@ -1,119 +1,54 @@
-import { liveQuery, type Subscription } from "dexie";
-import { shallowRef, getCurrentScope, onScopeDispose, watch, type ShallowRef, type WatchOptions } from "vue";
+import {liveQuery, type Subscription} from 'dexie'
+import {shallowRef, getCurrentScope, onScopeDispose, type ShallowRef} from 'vue'
 
-
-type Value<T, I> = I extends undefined ? T | undefined : T | I;
-
-type UseDexieLiveQueryWithDepsOptions<I, Immediate> = {
-  onError?: (error: any) => void;
-  initialValue?: I;
-} & WatchOptions<Immediate>;
-
-type UseDexieLiveQueryOptions<I> = {
-  onError?: (error: any) => void;
-  initialValue?: I;
-};
-
+type UseDexieLiveQueryOptions = {
+    onError?: (error: Error | unknown) => void
+}
 
 function tryOnScopeDispose(fn: () => void) {
-  if (getCurrentScope())
-    onScopeDispose(fn);
+    if (getCurrentScope()) onScopeDispose(fn)
 }
 
+export async function useDexieLiveQuery<T>(
+    querier: () => T | Promise<T>,
+    options: UseDexieLiveQueryOptions = {},
+): Promise<ShallowRef<T>> {
+    const {onError} = options
 
-export function useDexieLiveQueryWithDeps<
-  T,
-  I = undefined,
-  Immediate extends Readonly<boolean> = true,
->(
-  deps: any,
-  querier: (...data: any) => T | Promise<T>,
-  options: UseDexieLiveQueryWithDepsOptions<I, Immediate> = {},
-): ShallowRef<Value<T, I>> {
+    let initialData: T
+    try {
+        initialData = await querier()
+    } catch (error) {
+        onError?.(error)
+        throw error
+    }
 
-  const { onError, initialValue, ...rest } = options;
+    const value = shallowRef<T>(initialData)
+    let subscription: Subscription | undefined = undefined
 
-  const value = shallowRef<T | I | undefined>(initialValue);
+    const start = () => {
+        subscription?.unsubscribe()
 
-  let subscription: Subscription | undefined = undefined;
+        const observable = liveQuery(querier)
 
-  function start(...data: any) {
-    subscription?.unsubscribe();
+        subscription = observable.subscribe({
+            next: (result) => {
+                value.value = result
+            },
+            error: (error) => {
+                onError?.(error)
+            },
+        })
+    }
 
-    const observable = liveQuery(() => querier(...data));
+    const cleanup = () => {
+        subscription?.unsubscribe()
+        // Set to undefined to avoid calling unsubscribe multiple times on a same subscription
+        subscription = undefined
+    }
 
-    subscription = observable.subscribe({
-      next: result => {
-        value.value = result;
-      },
-      error: error => {
-        onError?.(error);
-      },
-    });    
-  }
+    start()
+    tryOnScopeDispose(cleanup)
 
-  function cleanup() {
-    subscription?.unsubscribe();
-
-    // Set to undefined to avoid calling unsubscribe multiple times on a same subscription
-    subscription = undefined;
-  }
-
-  watch(deps, start, { immediate: true, ...rest });
-
-  tryOnScopeDispose(() => {
-    cleanup();
-  });
-
-
-  return value as ShallowRef<Value<T, I>>;
-
-}
-
-
-export function useDexieLiveQuery<
-  T,
-  I = undefined,
->(
-  querier: () => T | Promise<T>,
-  options: UseDexieLiveQueryOptions<I> = {},
-): ShallowRef<Value<T, I>> {
-
-  const { onError, initialValue } = options;
-
-  const value = shallowRef<T | I | undefined>(initialValue);
-
-  let subscription: Subscription | undefined = undefined;
-
-  function start() {
-    subscription?.unsubscribe();
-
-    const observable = liveQuery(querier);
-
-    subscription = observable.subscribe({
-      next: result => {
-        value.value = result;
-      },
-      error: error => {
-        onError?.(error);
-      },
-    });    
-  }
-
-  function cleanup() {
-    subscription?.unsubscribe();
-
-    // Set to undefined to avoid calling unsubscribe multiple times on a same subscription
-    subscription = undefined;
-  }
-
-  start();
-
-  tryOnScopeDispose(() => {
-    cleanup();
-  });
-
-
-  return value as ShallowRef<Value<T, I>>;
-
+    return value
 }
